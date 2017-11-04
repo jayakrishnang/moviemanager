6096b3f5aa8e39490462ad0a8db84d26fc0e9d44
Jayakrishnan Gopal <jkdesignlab@gmail.com>
2017-11-05 00:59:52 +0530

    UserModule : UI changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,32 @@
 class MoviesController < ApplicationController
   def index
-  	@movies = Movie.all.order(:title)
+    @movies = Movie.select('movies.*, activity').with_user_activity(current_user.try(:id)).order(:title)
+  end
+
+  def unwatched_movies
+    @movies = Movie.select('movies.*, activity').with_user_activity(current_user.try(:id))
+                .where('activity IS NULL').order(:title)
+  end
+
+  def recommended_movies
+    @movies = RecommendationEngine.new(current_user.try(:id)).get_recommended_movies
+  end
+
+  def recommended_unwatched_movies
+    @movies = RecommendationEngine.new(current_user.try(:id)).get_unwatched_recommended_movies
   end
 
   def show
-  	@movie = Movie.where(id: params[:id]).first
+    @movie = Movie.select('movies.*, activity').with_user_activity(current_user.try(:id)).where(id: params[:id]).first
   end
 
   def list_years
-  	@years = Movie.pluck(:year).compact.uniq.sort.reverse
+    @years = Movie.pluck(:year).compact.uniq.sort.reverse
   end
 
   def list_movies_by_year
-  	@year = params['year']
-  	@movies = Movie.where(year: @year)
+    @year = params['year']
+    @movies = Movie.select('movies.*, activity').with_user_activity(current_user.try(:id)).where(year: @year)
   end
 
   def open_movie_folder
@@ -33,11 +46,19 @@ class MoviesController < ApplicationController
 
   def download_movie_file
     movie = Movie.where(id: params[:movie_id]).first
-    home_folder = "/media/rmed178lt/Seagate\ Backup\ Plus\ Drive/Movies/English/"
-    send_file("#{home_folder}#{movie.location}#{movie.file_name}", disposition: "attachment;filename=#{movie.file_name}")
-    # p "#{movie.location}/#{movie.file_name}"
-    # send_file "#{movie.location}/#{movie.file_name}",type: "application/jpg", disposition: 'attachment'
-    # send_file '/home/rmed178lt/Documents/eraop.csv', type: "text/csv", stream: false, disposition: "attachment;filename=eraop.csv"
+    home_folder = "#{Rails.root}/videos/English/"
+    send_file("#{home_folder}#{movie.location}#{movie.file_name}", disposition: "attachment;filename=#{movie.file_name}", stream: true)
     puts 'hi'
+  end
+
+  def toggle_marked_status
+    movie = Movie.select(:id).where(id: params[:movie_id]).first
+    unless UserActivity.exists?(user_id: current_user.id, movie_id: movie.id)
+      UserActivity.create(user_id: current_user.id, movie_id: movie.id, activity: 'Seen')
+    else
+      activity = UserActivity.where(user_id: current_user.id, movie_id: movie.id).first
+      activity.destroy if activity.present?
+    end
+    render json: {status: 'success'}
   end
 end
