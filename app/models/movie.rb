771bc6a41d135203f6b992eb82394e535dd4fcb0
Jayakrishnan Gopal <jkdesignlab@gmail.com>
2017-11-05 00:55:45 +0530

    UserModule : user activities

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
 class Movie < ActiveRecord::Base
 	has_many :movies_genres
 	has_many :genres, through: :movies_genres
+	has_many :user_activities
 	belongs_to :franchise
+	scope :with_user_activity, lambda { |user_id|
+						joins("LEFT OUTER JOIN user_activities
+										ON user_activities.movie_id = movies.id AND user_id=#{user_id}") }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :user_activities
+
+  def fetch_most_watched_genres
+    genres = User.select('COUNT(*) AS genre_count, genres.name')
+              .joins("INNER JOIN user_activities ON user_activities.user_id = users.id AND user_activities.user_id = #{id}")
+              .joins('INNER JOIN movies ON user_activities.movie_id = movies.id')
+              .joins('INNER JOIN movies_genres ON movies_genres.movie_id = movies.id')
+              .joins('INNER JOIN genres ON movies_genres.genre_id = genres.id')
+              .group('genres.id')
+    genres.map{|genre| {genre.name => genre.genre_count}}.reduce({}, :merge)
+  end
 end
